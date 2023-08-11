fedf6969c667de825e6f5e74ac1f5da352e58a3e
Sean Orelli <sorelli@arculus.co>
2023-08-11 11:22:13 -0400

    changed to use appl swift collection

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["DCBOR"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/wolfmcnally/swift-collections", .upToNextMajor(from: "1.1.0")),
+        .package(url: "https://github.com/apple/swift-atomics.git", .upToNextMajor(from: "1.0.0")),
         .package(url: "https://github.com/wolfmcnally/WolfBase.git", .upToNextMajor(from: "5.0.0")),
         .package(url: "https://github.com/blockchaincommons/BCSwiftFloat16.git", .upToNextMajor(from: "0.1.1")),
     ],
